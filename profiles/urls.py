1fd3901e7866b4ceecaa7a12700c2a197caf1f48
vivienrauch <vivienmirjam@gmail.com>
2023-07-10 11:05:29 +0000

    serializers

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from profiles import views
 
-urlpattern = [
+urlpatterns = [
     path('profiles/', views.ProfileList.as_view()),
+    path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
 ]
